f99a49ea6c56839b9059133dbb8128100d2ef292
johan <harryjohn026@gmail.com>
2021-06-04 00:04:44 +0530

    helll

--- app/views.py
+++ app/views.py
@@ -9,6 +9,8 @@ from django.views import View
 from .forms import CustomerProfileForm, RegistrationForm,LoginForm
 from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 
@@ -184,7 +186,7 @@ def removecart(request):
 
 
 
-
+@method_decorator(login_required,name='dispatch')
 class ProfileView(View):
     def get(self,request):
         form = CustomerProfileForm()
